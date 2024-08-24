5dd171d8f33a940892e73a7f0230e011d83ccde5
Ali İbrahim Keçeci <aibrahimkcc@gmail.com>
2024-08-25 02:21:56 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace UyakBul
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "uyak bul";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(49, 176);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(280, 308);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(356, 176);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(280, 308);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(675, 176);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(280, 308);
+            this.listBox3.TabIndex = 4;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(974, 176);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(280, 308);
+            this.listBox4.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1080, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Harf Kısıtı";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "listBox1",
+            "listBox2",
+            "listBox3"});
+            this.comboBox1.Location = new System.Drawing.Point(974, 103);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(706, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(24, 22);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(974, 135);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Kelime Sonu Eşleşmesi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(435, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Kelime İçi Eşlemesi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(749, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "son harf istenilen indexte";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1102, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Liste";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(289, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Kelimede geçmesi istediğiniz sıralı harfleri yazın";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1322, 649);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "UyakBul";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+
+namespace UyakBul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Person
+        {
+
+            public string Column1 { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+            string filePath = @"TDK.csv";
+            StreamReader reader = new StreamReader(filePath);
+
+            // harfler içinde var mı?
+            using (var csv = new CsvHelper.CsvReader(reader, new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                var records = csv.GetRecords<Person>().ToList();
+
+                // Filter records based on textBox1.Text
+                var filteredRecords = records.Where(k => k.Column1.Contains(textBox1.Text)).ToList();
+
+                // Add filtered records to listBox5
+                foreach (var record in filteredRecords)
+                {
+                    listBox2.Items.Add(record.Column1); // Assuming you're interested in adding the name
+                }
+
+                // Filter records based on textBox1.Text
+                var filteredRecordsEndsWith = records.Where(k => k.Column1.EndsWith(textBox1.Text)).ToList();
+
+                // Add filtered records to listBox5
+                foreach (var record in filteredRecordsEndsWith)
+                {
+                    listBox1.Items.Add(record.Column1); // Assuming you're interested in adding the name
+                }
+            }
+            // Aranan harflerin içinde bulunduğu kelimelerden oluşan bir array oluşturur.
+            int itemCount = listBox2.Items.Count;
+            string[] itemsArray = new string[itemCount];
+            for (int i = 0; i < itemCount; i++)
+            {
+                itemsArray[i] = listBox2.Items[i].ToString();
+
+
+            }
+            //aranan sıralı harflarin kelimenin hangi son harfine denk geleceği bilgisini alır.
+            int iindex = Convert.ToInt16(textBox2.Text);
+            foreach (string item in itemsArray)
+            {
+                int kelimenLength = (item.Length);//Aranılan kelime uzunluğu
+                int uyaklength = (textBox1.Text.Length);//aranılan harflerin uzunluğu
+                if (uyaklength + iindex > kelimenLength)
+                {
+                    continue;
+                }
+                // eğer aranılan harflerin tamamını bulursa x'in değeri aranılan harf sayısı kadar olacaktır ki bu doğrulama sayesinde bulunan kelime listeye yazdırılır.
+                int x = 0;
+                for (int j = 0; j < uyaklength; j++)
+                {
+ 
+                    //ilk seferinde aranılan harflerin sonuncusun değerini döndürür.
+                    //ilk seferinde son harfin olması istenilen indexdeki harfi döndürür.
+                    // harf sayısınca bu işlem devam eder.
+                    //Ali Keçecinin ilk algoritması!
+                    if (textBox1.Text[(uyaklength - 1 - j)] == item[kelimenLength - 1 - iindex - j])
+                    {
+                        x++;
+                    }
+                }
+                
+                if (x == uyaklength)
+                {
+                    listBox3.Items.Add(item);
+                }
+
+
+
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "listBox1")
+            {
+                foreach (var item in listBox1.Items)
+                {
+                    // Convert item to a string (or appropriate type)
+                    string itemText = item.ToString();
+                    if (itemText.Length == Convert.ToInt16(textBox3.Text))
+                    {
+                        listBox4.Items.Add(itemText);
+                    }
+
+                }
+            }
+            if (comboBox1.Text == "listBox2")
+            {
+                foreach (var item in listBox2.Items)
+                {
+                    // Convert item to a string (or appropriate type)
+                    string itemText = item.ToString();
+                    if (itemText.Length == Convert.ToInt16(textBox3.Text))
+                    {
+                        listBox4.Items.Add(itemText);
+                    }
+
+                }
+            }
+            if (comboBox1.Text == "listBox3")
+            {
+                foreach (var item in listBox3.Items)
+                {
+                    // Convert item to a string (or appropriate type)
+                    string itemText = item.ToString();
+                    if (itemText.Length == Convert.ToInt16(textBox3.Text))
+                    {
+                        listBox4.Items.Add(itemText);
+                    }
+
+                }
+            }
+
+        }
+    }
+}
